437de0573b286f1de07acbabfe07f786d196baad
Bill <tvkhue96@gmail.com>
2023-04-25 16:10:44 +0700

    another way to import fonts

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,10 @@ import { Head, Html, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="none" data-theme="none">
-      <Head />
+      <Head>
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Bangers&display=swap" />
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Space+Mono&display=swap" />
+      </Head>
       <body>
         <Main />
         <NextScript />
